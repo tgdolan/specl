89e12f8d7a1f29a8a99a81f5b4733d57810eec05
Tom Dolan <tgdolan@gmail.com>
2020-02-28 20:38:21 -0500

    csci-utils: Remove commented code.

--- io.py
+++ io.py
@@ -2,6 +2,7 @@
 # without exposing them publicly or to avoid naming conflicts!
 from contextlib import contextmanager
 from pathlib import Path
+import os
 
 from atomicwrites import atomic_write as _backend_writer, AtomicWriter
 
@@ -12,16 +13,9 @@ class SuffixWriter(AtomicWriter):
     def __init__(self, path, mode='w', overwrite=False,
                  **open_kwargs):
         self._path = path
-        # self._open_kwargs = open_kwargs
-        # self._path = path
-        # self._mode = mode
-        # self._overwrite = overwrite
         super().__init__(path, mode, overwrite, **open_kwargs)
 
     def get_fileobject(self, dir=None, **kwargs):
-        def __enter__(gf_self):
-            return gf_self
-
         temp_suffix = ''.join(Path(self._path).suffixes)
         super().get_fileobject(suffix=temp_suffix, **kwargs)
 
@@ -32,21 +26,28 @@ class SuffixWriter(AtomicWriter):
 
 @contextmanager
 def atomic_write(file, mode='w', as_file=True, new_default='asdf', **kwargs):
-    try:
+
         # You can override things just fine...
         with _backend_writer(file, writer_cls=AtomicWriter, **kwargs) as f:
-            # Don't forget to handle the as_file logic!
-            if not as_file:
-                yield f.name
-            else:
-                yield f
+            try:
+                # Don't forget to handle the as_file logic!
+                if not as_file:
+                    yield f.name
+                else:
+                    yield f
 
-    except (OSError, IOError):
-        # remove_temp_file(temp_file)
-        raise
+            except (OSError, IOError):
+                remove_temp_file(f.name)
+                raise
 
-    finally:
-        # remove_temp_file(temp_file)
-        pass
+            finally:
+                remove_temp_file(f.name)
 
+
+def remove_temp_file(file_path):
+    """ Removes the file designated by the 'file_path' param if it exists.
+    :param file_path: The file system path to the file to be removed.
+    """
+    if os.path.exists(file_path):
+        os.remove(file_path)
 
