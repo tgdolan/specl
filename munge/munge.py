5013138dfbe0bf72247ab860bdb2522986f9c91f
Tom Dolan <tgdolan@gmail.com>
2020-04-23 10:10:37 -0400

    munge: load_spec: Add tests/impl for reading YAML file into dictionary. Interim commit.

--- munge/munge.py
+++ munge/munge.py
@@ -1,1 +1,12 @@
 """Main module."""
+from yaml import load, FullLoader
+
+
+def read_spec(path: str) -> dict:
+    """Reads in a munge spec file and creates a dict from it.
+    This dict will be used to provide arguments to the various
+    data munging functions."""
+    with open(path) as yaml_spec:
+        spec = load(yaml_spec, Loader=FullLoader)
+
+    return spec if type(spec) == 'dict' else {}
--- tests/test_munge.py
+++ tests/test_munge.py
@@ -3,10 +3,41 @@
 """Tests for `munge` package."""
 
 import pytest
+from unittest.mock import patch, mock_open
 
+from munge import read_spec
 
-from munge import munge
+@pytest.fixture
+def empty_spec():
+    return ''
 
+@pytest.fixture
+def basic_spec():
+    return """
+    ---
+    input:
+      column_a:
+        data_type: int
+        name: COLUMN_A
+      column_b:
+        data_type: int
+        name: COLUMN_B
+      column_c:
+        data_type: string
+        name: COLUMN_C
+      column_d:
+        composed_of:
+          cols:
+            - column_a
+            - column_b
+          operation: multiply
+        name: COLUMN_D
+      file: source.csv
+    output:
+      file: out.csv
+      column_c
+      column_d
+    """
 
 @pytest.fixture
 def response():
@@ -16,6 +47,21 @@ def response():
     """
     # import requests
     # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+
+
+def test_that_load_spec_returns_empty_dict_for_empty_spec(empty_spec):
+
+    with patch('builtins.open', new_callable=mock_open, read_data=empty_spec):
+        spec = read_spec('fake/file.yaml')
+
+    assert (spec == {})
+
+
+def test_that_load_spec_returns_empty_dict_for_basic_spec(basic_spec):
+    with patch('builtins.open', new_callable=mock_open, read_data=basic_spec):
+        spec = read_spec('fake/file.yaml')
+
+    assert (spec == {})
 
 
 def test_content(response):
