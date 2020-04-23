03e13cfbe08a5cde665bbc0b707d168a6562496a
Tom Dolan <tgdolan@gmail.com>
2020-04-23 11:57:49 -0400

    munge: load_spec: Add assertions for basic spec load.

--- tests/test_munge.py
+++ tests/test_munge.py
@@ -16,12 +16,30 @@ def empty_spec():
 @pytest.fixture
 def basic_spec():
     return """
-cities:
-  - Bratislava
-  - Kosice
-  - Trnava
-  - Moldava
-  - Trencin
+input:
+  column_a:
+    data_type: int
+    name: COLUMN_A
+  column_b:
+    data_type: int
+    name: COLUMN_B
+  column_c:
+    data_type: string
+    name: COLUMN_C
+  column_d:
+    composed_of:
+      cols:
+        - column_a
+        - column_b
+      operation: multiply
+    name: COLUMN_D
+  file: source.csv
+output:
+  file: out.csv
+  COLUMN_C:
+    data_type: int
+  COLUMN_D:
+    data_type: int
 """
 
 
@@ -54,31 +72,24 @@ def basic_spec_0():
     """
 
 
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
-
-
 def test_that_load_spec_returns_empty_dict_for_empty_spec(empty_spec):
     with patch('builtins.open', new_callable=mock_open, read_data=empty_spec):
         spec = read_spec('fake/file.yaml')
 
-    assert (spec == {})
+    assert spec == {}
 
 
 def test_that_load_spec_returns_dict_for_basic_spec(basic_spec):
     with patch('builtins.open', new_callable=mock_open, read_data=basic_spec):
         spec = read_spec('fake/file.yaml')
 
-    assert (spec != {})
+    assert spec != {}, "Spec should not be empty for valid YAML file."
+    assert [*spec] == ['input', 'output']
+    assert spec['input']['file'] == 'source.csv'
+    assert spec['output']['file'] == 'out.csv'
 
 
-def test_that_load_spec_raises_ValueError_for_invalid_spec(basic_spec_0):
+def test_that_load_spec_raises_valueerror_for_invalid_spec(basic_spec_0):
     with pytest.raises(ValueError) as spec_error:
         with patch('builtins.open', new_callable=mock_open, read_data=basic_spec_0):
             spec = read_spec('fake/file.yaml')
@@ -86,7 +97,3 @@ def test_that_load_spec_raises_ValueError_for_invalid_spec(basic_spec_0):
     assert "invalid spec" in str(spec_error.value).lower()
 
 
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
