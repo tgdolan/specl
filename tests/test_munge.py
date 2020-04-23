4bcba675427ba2be74b33812d1093da037cd5a86
Tom Dolan <tgdolan@gmail.com>
2020-04-23 10:47:21 -0400

    munge: load_spec: Add support for multiple YAML documents in spec file.
    Utilize yaml.load_all for reading spec file to so that input/output could be
    defined as separate yml docs.
    
    Note: Indentation matters when stubbing out YAML strings for test fixtures.

--- tests/test_munge.py
+++ tests/test_munge.py
@@ -7,19 +7,21 @@ from unittest.mock import patch, mock_open
 
 from munge import read_spec
 
+
 @pytest.fixture
 def empty_spec():
     return ''
 
+
 @pytest.fixture
-def basic_spec_0():
+def basic_spec():
     return """
-    cities:
-      - Bratislava
-      - Kosice
-      - Trnava
-      - Moldava
-      - Trencin
+cities:
+  - Bratislava
+  - Kosice
+  - Trnava
+  - Moldava
+  - Trencin
 ---
 companies:
   - Eset
@@ -28,10 +30,11 @@ companies:
   - Matador Puchov
     """
 
+
 @pytest.fixture
-def basic_spec():
+def basic_spec_0():
     return """
-    ---
+ ---
     input:
       column_a:
         data_type: int
@@ -56,6 +59,7 @@ def basic_spec():
       column_d
     """
 
+
 @pytest.fixture
 def response():
     """Sample pytest fixture.
@@ -67,21 +71,20 @@ def response():
 
 
 def test_that_load_spec_returns_empty_dict_for_empty_spec(empty_spec):
-
     with patch('builtins.open', new_callable=mock_open, read_data=empty_spec):
         spec = read_spec('fake/file.yaml')
 
     assert (spec == {})
 
 
-def test_that_load_spec_returns_empty_dict_for_basic_spec(basic_spec_0):
-    with patch('builtins.open', new_callable=mock_open, read_data=basic_spec_0):
+def test_that_load_spec_returns_dict_for_basic_spec(basic_spec):
+    with patch('builtins.open', new_callable=mock_open, read_data=basic_spec):
         spec = read_spec('fake/file.yaml')
 
-    assert (spec == {})
+    assert (spec != {})
+
 
 def test_that_load_spec_raises_ValueError_for_invalid_spec(basic_spec_0):
-
     with pytest.raises(ValueError) as spec_error:
         with patch('builtins.open', new_callable=mock_open, read_data=basic_spec_0):
             spec = read_spec('fake/file.yaml')
